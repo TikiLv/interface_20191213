a405a23b11accdbd7456257a36f59475194c64ff
[594582910@qq.com] <[594582910@qq.com]>
2019-08-07 15:52:07 +0800

    调整文件位置，添加重新读取配置文件功能

---
+++ testCase/school/groupManager/testEditGroupManager.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import paramunittest
+import readConfig
+from common1 import Log
+from common1 import common
+from common1 import configHttp as ConfigHttp
+from common1 import businessCommon
+
+addGroupManager = common.get_xls("userCase.xls", "editGroupManager")
+localReadConfig = readConfig.ReadConfig()
+configHttp = ConfigHttp.ConfigHttp()
+
+@paramunittest.parametrized(*addGroupManager)
+class EditGroupManager(unittest.TestCase):
+    def setParameters(self,case_name, name, remarks, authoritys, code, msg):
+        '''
+
+        :param case_name:
+        :param name:
+        :param remarks:
+        :param authoritys:
+        :param code:
+        :param msg:
+        :return:
+        '''
+        self.case_name = str(case_name)
+        self.name = str(name)
+        self.remarks = str(remarks)
+        self.authoritys = str(authoritys)
+        self.code = int(code)
+        self.msg = msg
+
+
+    def setUp(self):
+        self.log = Log.MyLog.get_log()
+        self.logger = self.log.get_logger()
+        self.cookie = localReadConfig.get_headers('cookie')
+
+    def GetGroupManagerContent(self):
+        """
+        获取用户组ID
+        :return:
+        """
+        self.get_url = common.get_url_from_xml('testGetGroupManager')
+        configHttp.set_url(self.get_url)
+        # print(self.get_url)
+
+        # set headers
+        cookie = str(self.cookie)
+        header = {"Cookie": cookie}
+        configHttp.set_headers(header)
+
+        # test interface
+        self.return_json = configHttp.post()
+        #登录超时则重新调用登录
+        if self.return_json.json()['code'] == 4:
+            businessCommon.login()
+            # 重新设置cookie
+            self.newcookie = localReadConfig.get_headers('cookie')
+            # set headers
+            newcookie = str(self.newcookie)
+            header = {"Cookie": newcookie}
+            configHttp.set_headers(header)
+            # 重新请求
+            self.return_json = configHttp.post()
+
+        self.info = self.return_json.json()
+        self.message = self.return_json.json()
+        # print(self.message)
+        try:
+            self.newid = self.message['data']['list'][0]['id']
+            return self.newid
+        except IndexError:
+            self.log.build_case_line(self.case_name, self.code,'用户组不存在')
+        # print(self.newid)
+
+    def testEditGroupManager(self):
+        """
+        修改用户组
+        :return:
+        """
+
+        #获取用户组id
+        self.GetGroupManagerContent()
+
+        #设置url
+        self.url = common.get_url_from_xml('testAddGroupManager')
+        configHttp.set_url(self.url)
+
+        #设置请求头
+        cookie = str(self.cookie)
+        header = {"cookie":cookie}
+        configHttp.set_headers(header)
+
+        #设置body
+        data = {"id":self.newid , "authoritys":self.authoritys ,"name":self.name ,"remarks":self.remarks}
+        configHttp.set_data(data)
+        print('第三步：设置发送请求的参数')
+
+        #测试接口
+        self.return_json = configHttp.post()
+        self.url = self.return_json.url
+        method = str(self.return_json.request)[int(str(self.return_json.request).find('['))+1:int(str(self.return_json.request).find(']'))]
+        print("第四步：发送请求\n\t\t请求方法：" + method)
+
+        self.checkResult()
+
+    def tearDown(self):
+        """
+
+        :return:
+        """
+        self.log.build_case_line(self.case_name, self.info['code'], self.info['msg'])
+        print("测试结束，输出log完结\n\n")
+
+    def checkResult(self):
+        """
+        check result
+        :return:
+        """
+        self.info = self.return_json.json()
+        common.show_return_msg(self.return_json)
+        print(type(self.code))
+
+        if self.info['code'] == 0:
+            self.assertEqual(self.info['code'],self.code)
+            self.assertEqual(self.info['msg'],self.msg)
+
+        elif self.info['code'] == 3:
+            self.assertEqual(self.info['code'],self.code)
+            self.assertEqual(self.info['msg'],self.msg)
+
+        else :
+            self.log.build_case_line(self.case_name, self.code, 'the code doesn\'t exist')
+
+
+if __name__ == '__main__':
+    EditGroupManager(unittest.TestCase)
---
+++ testCase/school/userManager/testAddUserManager.py
@@ -0,0 +1,158 @@
+import unittest
+import paramunittest
+import readConfig as readConfig
+from common1 import Log as Log
+from common1 import common
+from common1 import configHttp as ConfigHttp
+from common1 import businessCommon
+
+addUserManager = common.get_xls("userCase.xls","addUserManager")
+localReadConfig = readConfig.ReadConfig()
+configHttp = ConfigHttp.ConfigHttp()
+info = {}
+
+@paramunittest.parametrized(*addUserManager)
+class AddUserManager(unittest.TestCase):
+    def setParameters(self, case_name, account, name, password, mobile, code, msg):
+        """
+
+        :param case_name:
+        :param account:
+        :param name:
+        :param password:
+        :param mobile:
+        :param code:
+        :param msg:
+        :return:
+        """
+        self.case_name = str(case_name)
+        self.account = str(account)
+        self.name = str(name)
+        self.password = str(password)
+        self.mobile = str(mobile)
+        self.code = int(code)
+        self.msg = str(msg)
+
+    def description(self):
+        """
+                test report description
+                :return:
+                """
+        return  self.case_name
+
+    def setUp(self):
+        """
+
+                :return:
+                """
+        self.log = Log.MyLog.get_log()
+        self.logger = self.log.get_logger()
+        self.cookie = localReadConfig.get_headers('cookie')
+
+
+    def testAddUser(self):
+        '''
+        set url
+        :return:
+        '''
+        #调用获取列表函数获取groupId
+        self.GetGroup()
+
+        self.url = common.get_url_from_xml('testAddUser')
+        configHttp.set_url(self.url)
+        print(self.url)
+
+        #set headers
+        cookie = str(self.cookie)
+        header = {"Cookie":cookie}
+        configHttp.set_headers(header)
+
+
+        #set data
+        if self.mobile == '':
+            data = { "account": self.account, "name": self.name, "password": self.password, "groupIds": self.groupIds }
+        else:
+            data = { "account": self.account, "name": self.name, "password": self.password, "groupIds": self.groupIds , "mobile":self.mobile }
+        configHttp.set_data(data)
+        print('第三步：设置发送请求的参数'+str(data))
+
+        #test interface
+        self.return_json = configHttp.post()
+        method = str(self.return_json.request)[int(str(self.return_json.request).find('['))+1:int(str(self.return_json.request).find(']'))]
+        print("第四步：发送请求\n\t\t请求方法：" + method)
+
+        #check result
+        self.checkResult()
+
+
+
+
+    def tearDown(self):
+        """
+
+        :return:
+        """
+        self.log.build_case_line(self.case_name, self.info['code'], self.info['msg'])
+        print("测试结束，输出log完结\n\n")
+
+    def GetGroup(self):
+        '''
+        获取用户组列表
+        :return:
+        '''
+        get_url = common.get_url_from_xml('testGetAllGroup')
+        configHttp.set_url(get_url)
+
+        #设置请求头
+        cookie = str(self.cookie)
+        header = {"Cookie": cookie}
+        configHttp.set_headers(header)
+
+        # test interface
+        self.return_json = configHttp.post()
+        #登录超时判断
+        if self.return_json.json()['code'] == 4:
+            businessCommon.login()
+            # 重新设置cookie
+            self.newcookie = localReadConfig.get_headers('cookie')
+            # set headers
+            newcookie = str(self.newcookie)
+            header = {"Cookie": newcookie}
+            configHttp.set_headers(header)
+            # 重新请求
+            self.return_json = configHttp.post()
+            print(self.return_json.json())
+
+        self.message = self.return_json.json()
+
+
+        #获取groupIds
+        try:
+            self.groupIds = self.message['data'][0]['id']
+            return self.groupIds
+        except IndexError:
+            self.log.build_case_line(self.case_name, self.code, '用户组不存在')
+
+    def checkResult(self):
+        """
+        check result
+        :return:
+        """
+        self.info = self.return_json.json()
+        print(self.info)
+        common.show_return_msg(self.return_json)
+
+        if self.info['code'] == 0:
+            self.assertEqual(self.info['code'],self.code)
+            self.assertEqual(self.info['msg'],self.msg)
+
+        elif self.info['code'] == 1:
+            self.assertEqual(self.info['code'],self.code)
+            self.assertEqual(self.info['msg'],self.msg)
+
+        else:
+            self.assertEqual(self.info['code'],[0,1])
+
+
+if __name__ == '__main__':
+    AddUserManager(unittest.TestCase)
