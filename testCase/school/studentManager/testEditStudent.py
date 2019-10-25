36d510fe5105516cb76009f7ec68513d1ac50f43
[594582910@qq.com] <[594582910@qq.com]>
2019-10-25 18:56:28 +0800

    Default

--- testCase/school/studentManager/testEditStudent.py
+++ testCase/school/studentManager/testEditStudent.py
@@ -1,6 +1,5 @@
 import unittest
 import paramunittest
-import time
 import readConfig as readConfig
 from common1 import Log
 from common1 import common
@@ -14,7 +13,7 @@ info ={}
 
 @paramunittest.parametrized(*editStudent)
 class editStudent(unittest.TestCase):
-    def setParameters(self,case_name, method, name, schoolNumber, startDate, gradeId, classId, address, code, msg):
+    def setParameters(self, case_name, method, name, schoolNumber, startDate, gradeId, classId, address, code, msg):
         '''
 
         :param case_name:
@@ -41,52 +40,159 @@ class editStudent(unittest.TestCase):
         self.code = int(code)
         self.msg = str(msg)
 
-        def description(self):
-            '''
+    def description(self):
+        '''
 
-            :param self:
-            :return:
-            '''
-            return case_name
+        :param self:
+        :return:
+        '''
+        return self.case_name
 
-        def setUp(self):
-            '''
+    def setUp(self):
+        '''
 
-            :return:
-            '''
-            self.log = Log.MyLog.get_log()
-            self.logger = self.log.get_logger()
-            self.cookie = localReadConfig.set_headers('cookie')
+        :return:
+        '''
+        self.log = Log.MyLog.get_log()
+        self.logger = self.log.get_logger()
+        self.cookie = localReadConfig.get_headers('cookie')
 
-        def getStudentId(self):
-            '''
-            获取学生ID
-            :param self:
-            :return:
-            '''
-            self.get_url = common.get_url_from_xml('getStudentId')
-            configHttp.set_url(self.get_url)
 
-            #设置请求头
-            cookie = str(self.cookie)
-            header = {'cookie':cookie}
+    def getStudentId(self):
+        '''
+        获取学生ID
+        :param self:
+        :return:z
+        '''
+        self.get_url = common.get_url_from_xml('getStudentId')
+        configHttp.set_url(self.get_url)
+
+        # 设置请求头
+        cookie = str(self.cookie)
+        header = {'cookie': cookie}
+        configHttp.set_headers(header)
+
+        # set data
+        data = {"pageNum": 1, "pageSize": 10}
+        configHttp.set_data(data)
+
+        # 测试接口
+        self.returnMessage_json = configHttp.post()
+        # 登录超时则重新调用登录
+        if self.returnMessage_json.json()['code'] == 4:
+            businessCommon.login()
+            # 重新设置cookie
+            self.newcookie = localReadConfig.get_headers('cookie')
+            # set headers
+            newcookie = str(self.newcookie)
+            header = {"Cookie": newcookie}
             configHttp.set_headers(header)
+            # 重新请求
+            self.returnMessage_json = configHttp.post()
 
-            #测试接口
+        self.info = self.returnMessage_json.json()
+
+        self.studentMessage = self.info['data']['list'][0]
+        # 判断列表是否有学生
+        try:
+            self.studentId = self.studentMessage['id']
+            self.schoolCode = self.studentMessage['cardCode']
+        except IndexError:
+            self.log.build_case_line(self.case_name, self.code, '学生列表为空')
+
+    def getGrade(self):
+        """
+        获取班级年级ID
+        :return:
+        """
+        self.get_url = common.get_url_from_xml("getGrade")
+        configHttp.set_url(self.get_url)
+
+        #设置请求头
+        cookie = str(self.cookie)
+        header = {"cookie":cookie}
+        configHttp.set_headers(header)
+
+        #发送请求
+        self.return_json = configHttp.post()
+        # 登录超时则重新调用登录
+        if self.return_json.json()['code'] == 4:
+            businessCommon.login()
+            # 重新设置cookie
+            self.newcookie = localReadConfig.get_headers('cookie')
+            # set headers
+            newcookie = str(self.newcookie)
+            header = {"Cookie": newcookie}
+            configHttp.set_headers(header)
+            # 重新请求
             self.return_json = configHttp.post()
-            # 登录超时则重新调用登录
-            if self.return_json.json()['code'] == 4:
-                businessCommon.login()
-                # 重新设置cookie
-                self.newcookie = localReadConfig.get_headers('cookie')
-                # set headers
-                newcookie = str(self.newcookie)
-                header = {"Cookie": newcookie}
-                configHttp.set_headers(header)
-                # 重新请求
-                self.return_json = configHttp.post()
-
-            self.info = self.return_json.json()
-            self.message = self.return_json.json()
 
 
+
+    def testEditStudent(self):
+        """
+        编辑学生
+        :param self:
+        :return:
+        """
+        self.getStudentId()
+        self.get_url = common.get_url_from_xml('editStudent')
+        configHttp.set_url(self.get_url)
+
+        # 设置请求头
+        cookie = str(self.cookie)
+        header = {'cookie': cookie}
+        configHttp.set_headers(header)
+
+        # set data
+        data = {"pageNum": 1, "pageSize": 10}
+        configHttp.set_data(data)
+
+        # 测试接口
+        self.return_json = configHttp.post()
+        # 登录超时则重新调用登录
+        if self.return_json.json()['code'] == 4:
+            businessCommon.login()
+            # 重新设置cookie
+            self.newcookie = localReadConfig.get_headers('cookie')
+            # set headers
+            newcookie = str(self.newcookie)
+            header = {"Cookie": newcookie}
+            configHttp.set_headers(header)
+            # 重新请求
+            self.return_json = configHttp.post()
+
+        self.info = self.return_json.json()
+
+    def tearDown(self):
+        """
+
+        :param self:
+        :return:
+        """
+        self.log.build_case_line(self.case_name, self.info['code'], self.info['msg'])
+        print("测试结束，输出log完结\n\n")
+
+    def checkResult(self):
+        """
+                check result
+                :return:
+                """
+        self.info = self.return_json.json()
+        common.show_return_msg(self.return_json)
+        print(self.info['msg'])
+
+        if self.info['code'] == 0:
+            self.assertEqual(self.info['code'], self.code)
+            self.assertEqual(self.info['msg'], self.msg)
+
+        elif self.info['code'] == 3:
+            self.assertEqual(self.info['code'], self.code)
+            self.assertEqual(self.info['msg'], self.msg)
+
+        else:
+            self.log.build_case_line(self.case_name, self.code, 'the code doesn\'t exist')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ testCase/school/studentManager/testGetCrade.py
@@ -0,0 +1,95 @@
+import unittest
+import paramunittest
+import readConfig as readConfig
+from common1 import Log
+from common1 import common
+from common1 import configHttp as ConfigHttp
+from common1 import businessCommon
+
+getGrade = common.get_xls("userCase.xls","getGrade")
+localReadConfig = readConfig.ReadConfig()
+configHttp = ConfigHttp.ConfigHttp()
+
+@paramunittest.parametrized(*getGrade)
+class getGrade(unittest.TestCase):
+    def setParameters(self, case_name, code, msg):
+        """
+
+        :param case_name:
+        :param code:
+        :param msg:
+        :return:
+        """
+        self.case_name = str(case_name)
+        self.code = int(code)
+        self.msg = str(msg)
+        self.gradeId = []
+
+    def description(self):
+        '''
+
+        :param self:
+        :return:
+        '''
+        return self.case_name
+
+    def setUp(self):
+        """
+
+        :return:
+        """
+        self.log = Log.MyLog.get_log()
+        self.logger = self.log.get_logger()
+        self.cookie = localReadConfig.get_headers('cookie')
+
+    def testGetGrade(self):
+        """
+        获取班级ID并写入excel
+        :return:
+        """
+        self.get_url = common.get_url_from_xml('getGrade')
+        configHttp.set_url(self.get_url)
+
+        #设置请求头
+        cookie = str(self.cookie)
+        header = {'cookies': cookie}
+        configHttp.set_headers(header)
+
+        #set data
+        data ={}
+        configHttp.set_data(data)
+
+        #test interface
+        self.return_json = configHttp.post()
+        # 登录超时则重新调用登录
+        if self.return_json.json()['code'] == 4:
+            businessCommon.login()
+            # 重新设置cookie
+            self.newcookie = localReadConfig.get_headers('cookie')
+            # set headers
+            newcookie = str(self.newcookie)
+            header = {"Cookie": newcookie}
+            configHttp.set_headers(header)
+            # 重新请求
+            self.return_json = configHttp.post()
+
+        self.info = self.return_json.json()
+
+        #判断请求是否成功
+        if self.info['code'] == 0:
+            self.assertEqual(self.info['code'], self.code)
+            self.assertEqual(self.info['msg'], self.msg)
+            #判断返回的gradeId是否为空，不为空则写入excel
+            self.allGrade = self.info['data']
+            if len(self.allGrade) != 0:
+                for i in range(len(self.allGrade)):
+                    self.gradeId.append(self.allGrade[i]['id'])
+                    print(self.gradeId)
+                common.write_xls('saveData.xls', 'grade',self.gradeId, 'gradeId')
+            else:
+                self.log.build_case_line(self.case_name, self.code, 'the Grade is null')
+        else:
+            self.log.build_case_line(self.case_name, self.code, 'the code doesn\'t exist')
+
+if __name__ == '__main__':
+    getGrade()
